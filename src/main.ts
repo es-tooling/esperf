bbb2453c16d2b756b355a669e71f986c07ddad62
43081j <43081j@users.noreply.github.com>
2024-08-07 19:39:28 +0100

    feat: add uninstall/autofix functionality
    Adds the following:
    
    - `fix` option to automatically run codemods against the matched files
    - `autoUninstall` option to automatically uninstall npm packages

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ import pc from 'picocolors';
 import {fdir} from 'fdir';
 import {getDocsUrl, getMdnUrl} from './replacement-urls.js';
 import {ts as sg} from '@ast-grep/napi';
-import {readFile} from 'node:fs/promises';
+import {readFile, writeFile} from 'node:fs/promises';
 import {extname} from 'node:path';
+import {codemods, type Codemod} from 'module-replacements-codemods';
+import {x} from 'tinyexec';
 
 const availableManifests: Record<string, modReplacements.ManifestModule> = {
   native: modReplacements.nativeReplacements,
@@ -113,17 +115,93 @@ function suggestReplacement(
   }
 }
 
+interface DependencyResult {
+  match: modReplacements.ModuleReplacement;
+  source: PackageSource['source'];
+}
+
 function traverseDependencies(
   dependencies: Record<string, string>,
   replacements: modReplacements.ModuleReplacement[],
   source: PackageSource['source']
-): void {
+): DependencyResult[] {
+  const results: DependencyResult[] = [];
+
   for (const key in dependencies) {
     for (const replacement of replacements) {
       if (key === replacement.moduleName) {
+        results.push({
+          match: replacement,
+          source
+        });
         suggestReplacement(replacement, {type: 'package', source});
       }
     }
+  }
+
+  return results;
+}
+
+interface ScanFileResult {
+  path: string;
+  contents: string;
+  matches: modReplacements.ModuleReplacement[];
+}
+
+async function fixFile(
+  scanResult: ScanFileResult,
+  cache: Record<string, Codemod>
+): Promise<void> {
+  let newContent = scanResult.contents;
+
+  for (const replacement of scanResult.matches) {
+    const factory = codemods[replacement.moduleName];
+
+    if (!factory) {
+      continue;
+    }
+
+    const cachedInstance = cache[replacement.moduleName];
+    let codemod;
+    if (!cachedInstance) {
+      codemod = factory({});
+      cache[replacement.moduleName] = codemod;
+    } else {
+      codemod = cachedInstance;
+    }
+
+    try {
+      const transformResult = await codemod.transform({
+        file: {
+          filename: scanResult.path,
+          source: newContent
+        }
+      });
+
+      newContent = transformResult;
+    } catch (err) {
+      cl.log.error(dedent`
+        ${pc.bold('Error:')} the ${pc.cyan(replacement.moduleName)} codemod unexpectedly threw an exception:
+
+        ${err}
+      `);
+    }
+  }
+
+  if (scanResult.contents !== newContent) {
+    cl.log.success(dedent`
+      Writing file ${scanResult.path} with codemod changes.
+    `);
+
+    await writeFile(scanResult.path, newContent, 'utf8');
+  }
+}
+
+async function fixFiles(scanResults: ScanFileResult[]): Promise<void> {
+  const codemodCache: Record<string, Codemod> = {};
+
+  for (const result of scanResults) {
+    await fixFile(result, codemodCache);
   }
 }
 
@@ -132,9 +210,14 @@ async function scanFile(
   contents: string,
   lines: string[],
   replacements: modReplacements.ModuleReplacement[]
-): Promise<void> {
+): Promise<ScanFileResult> {
   const ast = sg.parse(contents);
   const root = ast.root();
+  const result: ScanFileResult = {
+    path: filePath,
+    contents,
+    matches: []
+  };
 
   for (const replacement of replacements) {
     const imports = root.findAll({
@@ -155,6 +238,10 @@ async function scanFile(
         ]
       }
     });
+
+    if (imports.length > 0) {
+      result.matches.push(replacement);
+    }
 
     for (const node of imports) {
       const range = node.range();
@@ -183,15 +270,40 @@ async function scanFile(
       });
     }
   }
+
+  return result;
 }
 
 const knownFileExtensions = new Set<string>(['.tsx', '.ts', '.js', '.jsx']);
 
-async function traverseFiles(
-  dirPath: string,
+async function scanFiles(
+  files: string[],
   replacements: modReplacements.ModuleReplacement[],
   spinner: ReturnType<typeof cl.spinner>
-): Promise<void> {
+): Promise<ScanFileResult[]> {
+  const results: ScanFileResult[] = [];
+
+  for (const file of files) {
+    try {
+      const contents = await readFile(file, 'utf8');
+      const lines = contents.split('\n');
+
+      spinner.message(`Scanning ${file}`);
+
+      results.push(await scanFile(file, contents, lines, replacements));
+    } catch (err) {
+      cl.log.error(dedent`
+        Could not read file ${file}:
+
+        ${String(err)}
+      `);
+    }
+  }
+
+  return results;
+}
+
+async function traverseFiles(dirPath: string): Promise<string[]> {
   const fileScanner = new fdir();
 
   fileScanner
@@ -201,24 +313,7 @@ async function traverseFiles(
       return !isDir && knownFileExtensions.has(extname(filePath));
     });
 
-  const files = await fileScanner.crawl(dirPath).withPromise();
-
-  for (const file of files) {
-    try {
-      const contents = await readFile(file, 'utf8');
-      const lines = contents.split('\n');
-
-      spinner.message(`Scanning ${file}`);
-
-      await scanFile(file, contents, lines, replacements);
-    } catch (err) {
-      cl.log.error(dedent`
-        Could not read file ${file}:
-
-        ${String(err)}
-      `);
-    }
-  }
+  return await fileScanner.crawl(dirPath).withPromise();
 }
 
 function isDependenciesLike(obj: unknown): obj is Record<string, string> {
@@ -291,6 +386,16 @@ const options = await cl.group(
       cl.text({
         message: `Which directory would you like to scan for files? (default: ${cwd()})`,
         defaultValue: cwd()
+      }),
+    fix: () =>
+      cl.confirm({
+        message: 'Automatically apply codemods?',
+        initialValue: false
+      }),
+    autoUninstall: () =>
+      cl.confirm({
+        message: 'Automatically uninstall packages?',
+        initialValue: false
       })
   },
   {
@@ -321,22 +426,42 @@ const packageScanSpinner = cl.spinner();
 packageScanSpinner.start('Scanning `package.json` dependencies');
 
 if (isDependenciesLike(packageDependencies)) {
-  traverseDependencies(
+  const traverseResults = traverseDependencies(
     packageDependencies,
     manifestReplacements,
     'dependencies'
   );
+
+  if (options.autoUninstall) {
+    const args = [
+      'rm',
+      '-S',
+      ...traverseResults.map((result) => result.match.moduleName)
+    ];
+
+    await x('npm', args);
+  }
 }
 
 if (
   options.includeDevDependencies &&
   isDependenciesLike(packageDevDependencies)
 ) {
-  traverseDependencies(
+  const traverseResults = traverseDependencies(
     packageDevDependencies,
     manifestReplacements,
     'devDependencies'
   );
+
+  if (options.autoUninstall) {
+    const args = [
+      'rm',
+      '-D',
+      ...traverseResults.map((result) => result.match.moduleName)
+    ];
+
+    await x('npm', args);
+  }
 }
 
 packageScanSpinner.stop('`package.json` dependencies scanned successfully.');
@@ -345,8 +470,18 @@ const fileScanSpinner = cl.spinner();
 
 fileScanSpinner.start('Scanning files');
 
-await traverseFiles(options.filesDir, manifestReplacements, fileScanSpinner);
+const files = await traverseFiles(options.filesDir);
+
+const scanFilesResult = await scanFiles(
+  files,
+  manifestReplacements,
+  fileScanSpinner
+);
 
 fileScanSpinner.stop('Scanned files successfully.');
+
+if (options.fix) {
+  await fixFiles(scanFilesResult);
+}
 
 cl.outro('All checks complete!');
